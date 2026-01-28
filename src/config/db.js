e36162707833b24aa39463c77667d9b0c11e1cbc
alexlison <alexlison910@gmail.com>
2026-01-28 19:18:07 +0530

    MongoDb issue Resolved

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
+    console.log("url-->",process.env.MONGO_URI)
     process.exit(1);
   }
 };
